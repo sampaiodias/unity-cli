973f1ac6f0490c5893b046c41d1be946822c196f
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2017-10-27 13:14:07 -0200

    Version 0.1.5
    > Added SendMessage module

---
+++ Assets/CommandLine/Scripts/Modules/CommandLineModuleSendMessage.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CommandLineModuleSendMessage : MonoBehaviour, ICommandLineModule {
+
+    public void Execute(string[] args)
+    {
+        switch (args[1].ToLower())
+        {
+            case "callall":
+                GameObject[] allObjectsOnScene = GameObject.FindObjectsOfType<GameObject>();
+                for (int i = 0; i < allObjectsOnScene.Length; i++)
+                {
+                    try
+                    {
+                        allObjectsOnScene[i].SendMessage(args[2]);
+                    }
+                    catch (System.Exception)
+                    {
+                    }                    
+                }                
+                break;
+            case "call":
+                GameObject obj = GameObject.Find(args[3]);
+                obj.SendMessage(args[2]);
+                break;
+            case "callTag":
+                GameObject objTag = GameObject.FindGameObjectWithTag(args[3]);
+                objTag.SendMessage(args[2]);
+                break;
+            case "help":
+            case "h":
+                Help();
+                break;
+        }
+    }
+
+    public void Help()
+    {
+        Debug.Log("Commands: call string:methodName string:gameObjectName; callByTag string:methodName string:gameObjectTag; callAll string:methodName");
+    }
+}
